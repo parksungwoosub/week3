64561e96e9d0cab604d937f306af7e65cc4e7388
parksungwoo0811 <127467004+parksungwoo0811@users.noreply.github.com>
2023-03-10 13:50:40 +0900

    Create README.py

---
+++ README.py
@@ -0,0 +1,13 @@
+계산기
+
+a = int(input("첫번째 숫자 : ")) 
+b = int(input("두번째 숫자 : ")) 
+
+result = a + b print 
+( a , "+" , b, "=", result)
+result = a - b print 
+( a , "-" , b, "=", result)
+result = a * b print  
+( a , "*" , b, "=", result)
+result = a / b print 
+( a , "/" , b, "=", result) 
